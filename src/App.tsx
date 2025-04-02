72e3c97b44b07ae536ba15e6919e0c627d60c4f5
KMusyimi <kennmusyimi97@gmail.com>
2025-04-02 22:05:02 +0300

    added redirects for add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import './App.css';
 import { projectAction } from './components/Modal';
-import { taskFormAction } from './components/TaskForm.tsx';
+import { addLoader, taskFormAction } from './components/TaskForm.tsx';
 import { fetcherAction } from './components/TasksWrapper.tsx';
 import AllTasks from "./Views/AllTasks.tsx";
 import StartPage, { introLoader, startPageAction } from './Views/StartPage';
 import Task, { taskLoader } from './Views/Task.tsx';
 import TaskLayout, { projectsLoader } from "./Views/TaskLayout.tsx";
+
+
 
 const router = createBrowserRouter(createRoutesFromElements(
     // all todos /todos
@@ -15,19 +17,16 @@ const router = createBrowserRouter(createRoutesFromElements(
     <Route path={ "/"} action = { projectAction } >
     <Route index element = {< StartPage />} loader = { introLoader } action = { startPageAction } />
         <Route path={ 'projects' } element = {< TaskLayout />} loader = { projectsLoader } >
-            <Route index element = {< AllTasks />} />
+            <Route index element = {< AllTasks />}/>
                 < Route path = ':todoId' element = {<> </>} action={fetcherAction}/ >
-                    <Route path='add' element = {<> </>} action={taskFormAction} / >
+                    <Route path='add' element = {<> </>} loader={addLoader} action={taskFormAction} / >
                         <Route path=':id/todo' element = {< Task />} loader = { taskLoader } />
-                            <Route path=':id/todo/:todoId' element = {<> </>} action={fetcherAction}/>
+                            <Route path=':id/todo/:todoId' element = {<> </>} action={fetcherAction}/ >
                                 </Route>
-
-                                </Route>));
-
+                                </Route>
+));
 function App() {
-    return (
-        <RouterProvider router= { router } />
-    )
+    return (<RouterProvider router= { router } />);
 }
 
 export default App;
--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,20 +1,20 @@
 /* eslint-disable react-refresh/only-export-components */
-import {JSX,  useEffect, useRef} from "react";
-import {ActionFunctionArgs, Form, redirect, useNavigation} from "react-router-dom";
-import {addTask} from "../api";
 import moment from "moment";
+import { JSX, useEffect, useRef } from "react";
+import { ActionFunctionArgs, Form, redirect, useNavigation } from "react-router-dom";
+import { addTask } from "../api";
 
 interface FormProps {
     id: string | undefined;
     projectName: string | undefined;
 }
 
-export async function taskFormAction({request}: ActionFunctionArgs) {
+export async function taskFormAction({ request }: ActionFunctionArgs) {
     try {
         const formData = await request.formData();
         const data = Object.fromEntries(formData);
         const projectId = formData.get('projectId') as string;
-    
+
         await addTask(data);
         return redirect(`../${projectId}/todo?submitted=true`);
 
@@ -23,11 +23,16 @@ export async function taskFormAction({request}: ActionFunctionArgs) {
     }
 }
 
-export default function TaskForm({id, projectName}: FormProps): JSX.Element {
+// eslint-disable-next-line @typescript-eslint/require-await
+export async function addLoader() {
+    return redirect('..');
+}
+
+export default function TaskForm({ id, projectName }: FormProps): JSX.Element {
     const formContainerRef = useRef<HTMLDivElement>(null);
     const navigation = useNavigation();
     const status = navigation.state;
-   
+
     useEffect(() => {
         const timer = setTimeout(() => {
             if (formContainerRef.current) {
@@ -48,22 +53,22 @@ export default function TaskForm({id, projectName}: FormProps): JSX.Element {
                 </hgroup>
             </header>
             <Form action={"/projects/add"} className={'task-form'} replace={true} method="post">
-                <input type="hidden" name="projectId" value={id}/>
+                <input type="hidden" name="projectId" value={id} />
                 <label htmlFor={'title'}> Title </label>
                 <input type={'text'}
-                        id={'title'}
-                        name={'title'}
-                        className={'form-input'}
-                        placeholder={'Eg. bake a cake..'}
-                        required/>
+                    id={'title'}
+                    name={'title'}
+                    className={'form-input'}
+                    placeholder={'Eg. bake a cake..'}
+                    required />
 
                 <label htmlFor={'dueDate'}> Due date </label>
                 <input type={'datetime-local'}
-                        id={'dueDate'}
-                        name={'dueDate'}
-                        className={'form-input'}
+                    id={'dueDate'}
+                    name={'dueDate'}
+                    className={'form-input'}
                     min={moment().format('YYYY-MM-DDTHH:mm')}
-                        required/>
+                    required />
 
                 <fieldset>
                     <legend>Select task priority</legend>
@@ -71,35 +76,35 @@ export default function TaskForm({id, projectName}: FormProps): JSX.Element {
 
                         <div className={'radio-container'}>
                             <input id={'high'} type={'radio'} name={'priority'} value={3}
-                                   style={{height: '25px', width: '25px', verticalAlign: 'middle'}
-                                   } required/>
+                                style={{ height: '25px', width: '25px', verticalAlign: 'middle' }
+                                } required />
                             <label htmlFor={'high'}> High </label>
                         </div>
                         <div className={'radio-container'}>
                             <input id={'medium'} type={'radio'} name={'priority'} value={2}
-                                   style={{height: '25px', width: '25px', verticalAlign: 'middle'}}/>
+                                style={{ height: '25px', width: '25px', verticalAlign: 'middle' }} />
                             < label htmlFor={'medium'}> Medium </label>
                         </div>
                         <div className={'radio-container'}>
                             <input id={'low'} type={'radio'} name={'priority'} value={1}
-                                   style={{height: '25px', width: '25px', verticalAlign: 'middle'}}/>
+                                style={{ height: '25px', width: '25px', verticalAlign: 'middle' }} />
                             < label htmlFor={'low'}> Low </label>
                         </div>
                     </div>
                 </fieldset>
                 <label htmlFor={'description'}> Description </label>
                 <textarea id={'description'}
-                           name={'description'}
-                           className={'form-textarea'}
-                           placeholder={'Write a brief description...'}
-                           maxLength={250} required
+                    name={'description'}
+                    className={'form-textarea'}
+                    placeholder={'Write a brief description...'}
+                    maxLength={250} required
                 > </textarea>
                 <label htmlFor={'notes'}> Notes </label>
                 <textarea id={'notes'}
-                           name={'notes'}
-                           className={'form-textarea'}
-                           placeholder={'Write a brief note...'}
-                           maxLength={100}
+                    name={'notes'}
+                    className={'form-textarea'}
+                    placeholder={'Write a brief note...'}
+                    maxLength={100}
                 > </textarea>
                 <button type="submit" className="add-btn" disabled={status === 'submitting'}> Add task</button>
             </Form>
