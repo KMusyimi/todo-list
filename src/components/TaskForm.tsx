24c786afc11717e8d7fef219174a92c6addd6195
KMusyimi <kennmusyimi97@gmail.com>
2025-05-12 11:47:50 +0300

    task form bug fixes

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -85,8 +85,8 @@ export default function TaskForm({
             id: '',
             title: '',
             status: 'active',
-            dueDate: moment().format('YYYY-MM-DD'),
-            dueTime: '00:00:00',
+            dueDate: '',
+            dueTime: '',
             priority: '',
             description: '',
             createdAt: '',
@@ -97,34 +97,30 @@ export default function TaskForm({
     const [formState, setFormState] = useState<MyProject>(task);
     const formRef = useRef<HTMLFormElement>(null);
     const [minTime, setMinTime] = useState('');
-    const [minDate] = useState(() => moment().format('YYYY-MM-DD'));
-    const navigation = useNavigation();
     const inputRef = useRef<HTMLInputElement | null>(null);
     const [dueDate, setDueDate] = useState(() => formState?.tasks[0]?.dueDate);
     const fetcher = useFetcher();
+    const navigation = useNavigation();
 
     const status = navigation.state;
 
     useEffect(() => {
+        const minDate = moment().format('YYYY-MM-DD');
         let timer: string | number | NodeJS.Timeout | undefined;
-        setMinTime((prev) => {
-            if (dueDate === minDate) {
-                prev = moment().format('HH:mm');
-            } else {
-                prev = ''
-            }
-            return prev;
-        });
+        
+        setMinTime(() => (dueDate === minDate ? moment().format('HH:mm') : ''));
+
         if (formIntent) {
-            let task;
+            let task: MyProject | undefined;
+            
             const { intent, projectId, taskId } = formIntent;
-            if (intent === 'edit') {
-                const regxp = new RegExp(`^.*${taskId ?? ''}.*$`);
+            if (intent === 'edit' && taskId) {
                 if (projects) {
                     const projectsCopy = [...projects]
                     for (const project of projectsCopy) {
                         if (project?.id === projectId) {
-                            const tasks = project?.tasks.filter(task => regxp.test(task?.id ?? ''));
+                            const regexp = new RegExp(`^.*${taskId}.*$`);
+                            const tasks = project?.tasks.filter(task => regexp.test(task?.id ?? ''));
                             task = { ...project, tasks } as MyProject;
                         }
                     }
@@ -134,11 +130,13 @@ export default function TaskForm({
                 }
             }
         }
+
         return () => {
             clearTimeout(timer);
         }
-    }, [status, toggleForm, setToggleForm, formIntent, projects, dueDate, minDate]);
-
+    }, [status, toggleForm, setToggleForm, formIntent, projects, dueDate]);
+   
+   
     const closeForm = useCallback(() => {
         setFormState(prev => {
             prev = task;
@@ -199,12 +197,12 @@ export default function TaskForm({
 
         }, 70);
     }, [closeForm]);
-
+    console.log(formState);
     return (
         <>
             <div className={'form-container'}>
-                <header><i style={{height:'35px'}}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5"
-                style={{height:"inherit"}}
+                <header><i style={{ height: '35px' }}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5"
+                    style={{ height: "inherit" }}
                 >
                     <path d="m5.433 13.917 1.262-3.155A4 4 0 0 1 7.58 9.42l6.92-6.918a2.121 2.121 0 0 1 3 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 0 1-.65-.65Z" />
                     <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0 0 10 3H4.75A2.75 2.75 0 0 0 2 5.75v9.5A2.75 2.75 0 0 0 4.75 18h9.5A2.75 2.75 0 0 0 17 15.25V10a.75.75 0 0 0-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5Z" />
@@ -264,7 +262,7 @@ export default function TaskForm({
                                 placeholder="mm/dd/yyyy"
                                 className={'form-input'}
                                 min={moment().format('YYYY-MM-DD')}
-                                value={dueDate}
+                                value={formState?.tasks[0]?.dueDate}
                                 onInput={handleOnInput}
                                 required />
 
