59b06b12e40db42d9aecf0ed7886e38c78da4063
KMusyimi <kennmusyimi97@gmail.com>
2025-05-13 11:48:39 +0300

    minor ui tweaks

--- src/Views/TaskDetails.tsx
+++ src/Views/TaskDetails.tsx
@@ -85,13 +85,13 @@ export default function TaskDetails(): JSX.Element {
         onTransitionEnd={handleTransitionEnd}
       >
 
-        <Link to={backToLink} relative={'path'}> <i>
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
-            <path d="M6.28 5.22a.75.75 0 0 0-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 1 0 1.06 1.06L10 11.06l3.72 3.72a.75.75 0 1 0 1.06-1.06L11.06 10l3.72-3.72a.75.75 0 0 0-1.06-1.06L10 8.94 6.28 5.22Z" />
-          </svg>
-
-        </i>
-
+        <Link to={backToLink} relative={'path'}>
+          <i>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
+              <path fillRule="evenodd" d="M17 10a.75.75 0 0 1-.75.75H5.612l4.158 3.96a.75.75 0 1 1-1.04 1.08l-5.5-5.25a.75.75 0 0 1 0-1.08l5.5-5.25a.75.75 0 1 1 1.04 1.08L5.612 9.25H16.25A.75.75 0 0 1 17 10Z" clipRule="evenodd" />
+            </svg>
+          </i>
+          <span>go back </span>
         </Link>
 
         <div className="header-container">
