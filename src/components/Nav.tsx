1d75bc9eacbdf3fdcd7edb8521fb3373dd72f533
KMusyimi <kennmusyimi97@gmail.com>
2025-05-14 12:33:57 +0300

    minor ui tweaks for safari

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -81,11 +81,9 @@ function Render({ projects, setModalIntent }: NavProps) {
                     {projectName}
                 </NavLink>
                 <button type="button" onClick={handleToggleOptions(idx)}>
-                    <i>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
-                            <path d="M10 3a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM10 8.5a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM11.5 15.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0Z" />
-                        </svg>
-                    </i>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
+                        <path d="M10 3a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM10 8.5a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM11.5 15.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0Z" />
+                    </svg>
                 </button>
                 <span className="count">{tasks?.length}</span>
                 {toggleItems[idx] && <div id={`menu-${id ?? ''}`} className={"project-menu"}>
